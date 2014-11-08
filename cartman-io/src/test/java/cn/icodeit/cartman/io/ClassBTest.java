9f78c5373df099d4a4aa2a308ed27fad25242d25
zhoucong1020 <zhoucong@zhoucongdeMacBook-Pro.local>
2014-11-09 01:24:35 +0800

    cartman-io test

--- cartman-io/src/test/java/cn/icodeit/cartman/io/ClassBTest.java
+++ cartman-io/src/test/java/cn/icodeit/cartman/io/ClassBTest.java
@@ -1,8 +1,14 @@
 package cn.icodeit.cartman.io;
 
+import org.junit.Test;
+
 /**
  * Created by zhoucong on 14/11/9.
  * cn.icodeit.cartman.io
  */
 public class ClassBTest {
+    @Test
+    public void testB(){
+
+    }
 }
