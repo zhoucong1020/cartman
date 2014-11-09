12ea9452539f7ab4cb3dd13a2c8917ddb4801612
zhoucong1020 <zhoucong@zhoucongdeMacBook-Pro.local>
2014-11-09 21:42:55 +0800

    create cartman-doc

---
+++ cartman-doc/src/main/java/cn/icodeit/cartman/doc/ClassC.java
@@ -0,0 +1,8 @@
+package cn.icodeit.cartman.doc;
+
+/**
+ * Created by zhoucong on 14/11/9.
+ * cn.icodeit.cartman.doc
+ */
+public class ClassC {
+}
---
+++ cartman-doc/src/test/java/cn/icodeit/cartman/doc/ClassCTest.java
@@ -0,0 +1,14 @@
+package cn.icodeit.cartman.doc;
+
+import org.junit.Test;
+
+/**
+ * Created by zhoucong on 14/11/9.
+ * cn.icodeit.cartman.doc
+ */
+public class ClassCTest {
+    @Test
+    public void testC(){
+
+    }
+}
