148ada2f0da0d16f67371ee42d605892a9751b82
yunguijingsan <yunguijingsan@github.com>
2014-12-01 18:09:38 +0800

    测试类
    测试类

---
+++ cartman-doc/src/main/java/cn/icodeit/cartman/doc/testService/ServiceA.java
@@ -0,0 +1,35 @@
+package cn.icodeit.cartman.doc.testService;
+
+import cn.icodeit.cartman.core.annotation.Mapping;
+import cn.icodeit.cartman.core.annotation.MethodField;
+import cn.icodeit.cartman.core.annotation.Param;
+import cn.icodeit.cartman.core.annotation.Service;
+import cn.icodeit.cartman.core.annotation.errorCode.ResponseCode;
+
+/**
+ * .
+ * User: yuanweifeng
+ * Date: 14-11-26
+ * Time: 上午10:51
+ */
+@Service(value = "/s001")
+public class ServiceA {
+
+    @Mapping(value = "tt",method = MethodField.GET, status = ResponseCode.success)
+    public String test(
+            @Param(value = "xyz", description = "")
+            String abc
+           /* @Param(value = "abc",description = "")
+            String aaq*/
+    ) {
+        return "test method" + abc + " :";
+    }
+
+
+    @Mapping(value = "ss", status = ResponseCode.success,method = MethodField.POST)
+    public String test(
+
+    ) {
+        return "test method ttttt";
+    }
+}
